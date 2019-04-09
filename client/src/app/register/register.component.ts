ed34223093f79085aa69ce6d984658ccf88f1402
pedrovivaldi <pedro.cotta.vivaldi@gmail.com>
2019-04-08 22:06:11 -0300

    Added nav bar and login & register pages

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+@Component({templateUrl: 'register.component.html'})
+export class RegisterComponent implements OnInit {
+    registerForm: FormGroup;
+    loading = false;
+    submitted = false;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router
+    ) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            email: ['', Validators.required],
+            phone: ['', Validators.required],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+    }
+}
