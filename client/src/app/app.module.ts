e842d8b8567066c86902a30b99eb72b28945cd26
Marcus <marcusbrocaneli@gmail.com>
2019-04-14 00:25:56 -0300

    Added 2 components: permissions, showing all users, and permissions-detail, showing selected user permission

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './register/register.component';
+import { PermissionsComponent } from './permissions/permissions.component';
+import { PermissionsDetailComponent } from './permissions-detail/permissions-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    PermissionsComponent,
+    PermissionsDetailComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/permissions/permissions.component.ts
+++ client/src/app/permissions/permissions.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-permissions',
   templateUrl: './permissions.component.html',
   styleUrls: ['./permissions.component.css']
 })
-export class PermissionsComponent implements OnInit {
+export class PermissionsComponent implements OnInit, OnChanges {
 
+  people = [{id:1, name: "Marcus"},
+             {id:2, name: "Julia"}, 
+             {id:3, name: "Marilia"}, 
+             {id:4, name: "Matheus"}, 
+             {id:5, name: "Pedro"}]
 
-  people = [{name: "Marcus"},
-             {name: "Julia"}, 
-             {name: "Marilia"}, 
-             {name: "Matheus"}, 
-             {name: "Pedro"}]
 
   constructor() { }
 
   ngOnInit() {
+    this.people.sort((a, b) => (a.name > b.name) ? 1 : -1)
+  }
+
+  ngOnChanges() {
+
   }
 
 }
