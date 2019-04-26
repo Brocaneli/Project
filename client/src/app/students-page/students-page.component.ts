b4f4871c3d5bc3f3e2d81fd3e56c1950c1c5e088
pedrovivaldi <pedro.cotta.vivaldi@gmail.com>
2019-04-25 21:06:07 -0300

    Improvements in Students Page

--- client/src/app/students-page/students-page.component.ts
+++ client/src/app/students-page/students-page.component.ts
@@ -7,21 +7,11 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class StudentsPageComponent implements OnInit {
 
-  add_class(){
-  }
-
-  cursos = [
-    {name : "Aula 1"},
-    {name : "Aula 2"},
-    {name : "Aula 3"},
-    {name : "Aula 4"}
-  ]
-
-  avisos = [
-    {name : "Aviso 1"},
-    {name : "Aviso 2"},
-    {name : "Aviso 3"},
-    {name : "Aviso 4"}
+  private avisos = [
+    { name: "Aviso 1", date: "12/01/18", user: "julia", comment: "hello world!" },
+    { name: "Aviso 2", date: "12/01/18", user: "julia", comment: "hello world!" },
+    { name: "Aviso 3", date: "12/01/18", user: "julia", comment: "hello world!" },
+    { name: "Aviso 4", date: "12/01/18", user: "julia", comment: "hello world!" }
   ]
 
   constructor() { }
