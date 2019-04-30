e1716d590e9fce5c19805a52b5b387e92cc71c5c
pedrovivaldi <pedro.cotta.vivaldi@gmail.com>
2019-04-29 22:22:47 -0300

    Implemented Login and Register

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import {Md5} from 'ts-md5/dist/md5';
+import { UserService } from '../user.service';
 
 @Component({templateUrl: 'register.component.html'})
 export class RegisterComponent implements OnInit {
@@ -11,7 +12,8 @@ export class RegisterComponent implements OnInit {
 
     constructor(
         private formBuilder: FormBuilder,
-        private router: Router
+        private router: Router,
+        private userService: UserService
     ) { }
 
     ngOnInit() {
@@ -19,7 +21,8 @@ export class RegisterComponent implements OnInit {
             name: ['', Validators.required],
             email: ['', Validators.required],
             phone: ['', Validators.required],
-            password: ['', [Validators.required, Validators.minLength(6)]]
+            password: ['', [Validators.required, Validators.minLength(6)]],
+            role: ['', Validators.required]
         });
     }
 
@@ -29,13 +32,24 @@ export class RegisterComponent implements OnInit {
     onSubmit() {
         this.submitted = true;
 
+        this.f.role.setValue('ALUNO');
+
         // stop here if form is invalid
         if (this.registerForm.invalid) {
             return;
         }
 
-        this.loading = true;
+        this.f.password.setValue(Md5.hashStr(this.f.password.value).toString());
 
-        this.router.navigate(['login']);
+        this.loading = true;
+        this.userService.createUser(this.registerForm.getRawValue()).subscribe(
+            (data) => {
+                this.router.navigate(['login']);
+            }, 
+            (error) => {
+                this.loading = false;
+            }
+        );
+
     }
 }
--- server/myapp/models.py
+++ server/myapp/models.py
@@ -22,7 +22,7 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=11)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
     role = models.CharField(max_length=15)
     created_at = models.DateTimeField(default=datetime.datetime.today)
 
