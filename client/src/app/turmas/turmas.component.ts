9852d2822204c930d4e7e4e79c10f8043d569012
unknown <mrmatheustor@gmail.com>
2019-05-02 21:02:24 -0300

    Turma Service added

--- client/src/app/turmas/turmas.component.ts
+++ client/src/app/turmas/turmas.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, ActivatedRoute } from '@angular/router';
+import { CicloService } from '../ciclo.service';
+import { TurmasService } from '../turmas.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './turmas.component.html',
@@ -7,15 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TurmasComponent implements OnInit {
 
-  cursos = [{nome: "Ciclo 01", desc1: "01 - Radiações e Vibrações"},
-{nome: "Ciclo 2"},
-{nome: "Ciclo 3"},
-{nome: "Assistência Espiritual"},
-{nome: "Mocidade Espírita"}];
+  turmaForm: FormGroup;
+  private turmas: any;
+  private ciclos: any;
 
-  constructor() { }
+
+
+  cursos = [{ nome: "Ciclo 01", desc1: "01 - Radiações e Vibrações" },
+  { nome: "Ciclo 2" },
+  { nome: "Ciclo 3" },
+  { nome: "Assistência Espiritual" },
+  { nome: "Mocidade Espírita" }];
+
+  constructor(private router: Router, private cicloService: CicloService, private fb: FormBuilder, private turmaService: TurmasService) { }
 
   ngOnInit() {
+
+    this.cicloService.getCiclos().subscribe(data => {
+      this.ciclos = data;
+    });
+
+    this.turmaService.getTurma().subscribe(data => {
+      this.turmas = data;
+    });
+
+    this.turmaForm = this.fb.group({
+      name: ['', Validators.required],
+      ciclo_id: [''],
+      start_date: [''],
+      end_date: ['']
+    });
+    
+    
+  }
+
+  onSubmit() {
+
+    console.log(this.turmaForm.getRawValue());
+    this.turmaService.createTurma(this.turmaForm.getRawValue()).subscribe(data => {
+    });
+
+    //window.location.reload();
+
+  }
+
+  deleteTurma(id: number) {
+    this.turmaService.deleteTurma(id).subscribe(data => {
+    });
+
+    window.location.reload();
   }
 
 }
